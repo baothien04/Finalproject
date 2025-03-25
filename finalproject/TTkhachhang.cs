332b8f6b78e1dfe0390606db55f568c3d918ba91
baothien04 <thiennsb22@uef.edu.vn>
2025-03-25 22:03:18 +0700

    TTkhachhang.cs

--- finalproject/TTkhachhang.cs
+++ finalproject/TTkhachhang.cs
@@ -19,7 +19,26 @@ namespace finalproject
 
         private void button5_Click(object sender, EventArgs e)
         {
+            MessageBox.Show("Displaying customer information.", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
 
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Navigating to Home.", "Navigation", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Displaying product details.", "Product", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Are you sure you want to log out?", "Confirm", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.Yes)
+            {
+                this.Close(); // Close the form
+            }
         }
     }
 }
