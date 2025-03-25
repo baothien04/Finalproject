7773645e62fc9c0ec5a685df160c63cf43b04b7c
baothien04 <thiennsb22@uef.edu.vn>
2025-03-25 22:00:29 +0700

    Form1.cs

--- finalproject/Form1.cs
+++ finalproject/Form1.cs
@@ -15,6 +15,46 @@ namespace finalproject
         public Form1()
         {
             InitializeComponent();
+            // Gán sự kiện cho checkbox và button
+            checkBox1.CheckedChanged += new EventHandler(checkBox1_CheckedChanged);
+            button1.Click += new EventHandler(button1_Click);
+            button2.Click += new EventHandler(button2_Click);
+        }
+
+        // Sự kiện checkbox để hiển thị/ẩn mật khẩu
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            textBox2.UseSystemPasswordChar = !checkBox1.Checked;
+        }
+
+        // Xử lý đăng nhập
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string username = textBox1.Text;
+            string password = textBox2.Text;
+
+            // Kiểm tra tài khoản (giả lập)
+            if (username == "admin" && password == "1234")
+            {
+                MessageBox.Show("Đăng nhập thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                
+                // Mở Form2 sau khi đăng nhập thành công
+                Form2 form2 = new Form2();
+                form2.Show();
+                this.Hide(); // Ẩn Form1
+            }
+            else
+            {
+                MessageBox.Show("Sai tài khoản hoặc mật khẩu!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // Xử lý khi bấm nút REGISTER
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FormRegister registerForm = new FormRegister();
+            registerForm.Show();
+            this.Hide();
         }
     }
 }
