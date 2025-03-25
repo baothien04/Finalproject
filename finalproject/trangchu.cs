4d1630b72792ebca4514bfdc365825a9a7f790dc
baothien04 <thiennsb22@uef.edu.vn>
2025-03-25 22:06:52 +0700

    Update trangchu.cs

--- finalproject/trangchu.cs
+++ finalproject/trangchu.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace finalproject
 {
     public partial class trangchu : Form
@@ -15,6 +16,44 @@ namespace finalproject
         public trangchu()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Xử lý đăng xuất
+            DialogResult result = MessageBox.Show("Bạn có chắc chắn muốn đăng xuất?", "Xác nhận", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.Yes)
+            {
+                this.Close(); // Đóng form hiện tại
+                // Mở lại form đăng nhập nếu cần
+                // new LoginForm().Show();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Xử lý khi nhấn nút HOME
+            MessageBox.Show("Bạn đang ở trang chủ", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // Mở form sản phẩm
+            ProductForm productForm = new ProductForm();
+            productForm.Show();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            // Hiển thị thông tin ứng dụng hoặc trợ giúp
+            MessageBox.Show("Ứng dụng Quản lý cửa hàng trang sức\nPhiên bản 1.0\nHỗ trợ: support@jewelryshop.com", "Giới thiệu", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            // Mở form thông tin khách hàng
+            CustomerInfoForm customerForm = new CustomerInfoForm();
+            customerForm.Show();
         }
     }
 }
