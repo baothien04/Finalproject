78273e1e73f36a1759250e58ba20d0b8550b2933
baothien04 <thiennsb22@uef.edu.vn>
2025-03-31 11:01:30 +0700

    Update trangchu.cs

--- finalproject/trangchu.cs
+++ finalproject/trangchu.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using System;
+using System.Windows.Forms;
 
 namespace finalproject
 {
@@ -20,40 +22,59 @@ namespace finalproject
 
         private void button1_Click(object sender, EventArgs e)
         {
-            // Xử lý đăng xuất
-            DialogResult result = MessageBox.Show("Bạn có chắc chắn muốn đăng xuất?", "Xác nhận", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
-            if (result == DialogResult.Yes)
-            {
-                this.Close(); // Đóng form hiện tại
-                // Mở lại form đăng nhập nếu cần
-                // new LoginForm().Show();
-            }
+            // Đăng xuất
+            this.Close(); // Đóng Form trangchu
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            // Xử lý khi nhấn nút HOME
-            MessageBox.Show("Bạn đang ở trang chủ", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            // Mở lại Form Trang Chủ (có thể không cần thiết nếu đây là trang chủ)
+            MessageBox.Show("Bạn đang ở trang chủ.", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            // Mở form sản phẩm
-            ProductForm productForm = new ProductForm();
-            productForm.Show();
-        }
-
-        private void button4_Click(object sender, EventArgs e)
-        {
-            // Hiển thị thông tin ứng dụng hoặc trợ giúp
-            MessageBox.Show("Ứng dụng Quản lý cửa hàng trang sức\nPhiên bản 1.0\nHỗ trợ: support@jewelryshop.com", "Giới thiệu", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            // Mở Form Quản Lý Sản Phẩm
+            sanpham sanpham = new sanpham();
+            this.Hide(); // Ẩn Form trangchu
+            sanpham.ShowDialog(); // Hiển thị Form Quản Lý Sản Phẩm như một hộp thoại
+            this.Show(); // Hiển thị lại Form trangchu nếu Form Quản Lý Sản Phẩm đóng
         }
 
         private void button5_Click(object sender, EventArgs e)
         {
-            // Mở form thông tin khách hàng
-            CustomerInfoForm customerForm = new CustomerInfoForm();
-            customerForm.Show();
+            // Mở Form Thông Tin Khách Hàng
+            TTkhachhang TTkhachhang = new TTkhachhang();
+            this.Hide(); // Ẩn Form trangchu
+            TTkhachhang.ShowDialog(); // Hiển thị Form Thông Tin Khách Hàng như một hộp thoại
+            this.Show(); // Hiển thị lại Form trangchu nếu Form Thông Tin Khách Hàng đóng
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {// Mở lại Form Trang Chủ (có thể không cần thiết nếu đây là trang chủ)
+            MessageBox.Show("Bạn đang ở trang chủ.", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.Close(); // Đóng Form trangchu
+        }
+
+        private void button5_Click_1(object sender, EventArgs e)
+        {
+            // Mở Form Thông Tin Khách Hàng
+            TTkhachhang TTkhachhang = new TTkhachhang();
+            this.Hide(); // Ẩn Form trangchu
+            TTkhachhang.ShowDialog(); // Hiển thị Form Thông Tin Khách Hàng như một hộp thoại
+            this.Show(); // Hiển thị lại Form trangchu nếu Form Thông Tin Khách Hàng đóng
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            sanpham sanpham = new sanpham();
+            this.Hide(); // Ẩn Form trangchu
+            sanpham.ShowDialog(); // Hiển thị Form Quản Lý Sản Phẩm như một hộp thoại
+            this.Show(); // Hiển thị lại Form trangchu nếu Form Quản Lý Sản Phẩm đóng
         }
     }
 }
